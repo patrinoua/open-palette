ef08528d846dc28c6d603699b297a2ee661dafd4
Mona Lisa <patrinoua@gmail.com>
2022-04-19 18:31:25 +0200

    Spacer stories

--- src/atoms/Spacer/Spacer.stories.js
+++ src/atoms/Spacer/Spacer.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 import { Text } from 'react-native'
-import { Separator } from './Separator'
+import { Spacer } from './Spacer'
 
 export default {
   title: 'Spacer',
